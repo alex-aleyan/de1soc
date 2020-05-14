d127b6837acfc32bca38817bb1581998be7be6d1
root <root@cobalt-vm.acct04.us.lmco.com>
2020-05-13 20:26:35 -0400

    misc

--- minimal/minimal_fpga_and_hps_001/sw/getting_started.c
+++ minimal/minimal_fpga_and_hps_001/sw/getting_started.c
@@ -11,17 +11,14 @@ int main(void)
 	/* Declare volatile pointers to I/O registers (volatile means that the locations
 	 * will not be cached, even in registers) */
 	volatile int * LED_ptr 				= (int *) PIO_0_LED_BASE;
-	int LED_ptr = 0x0000000F;					// initial pattern for HEX displays
 	volatile int delay_count;					// volatile so the C compiler doesn't remove the loop
 
 	while (1)
 	{
-		/* rotate the pattern shown on the HEX displays */
-		if (LED_ptr & 0x80000000)
-			LED_ptr = (LED_ptr << 1) | 1;
-		else
-			LED_ptr = LED_ptr << 1;
+		for (delay_count = 250000; delay_count != 0; --delay_count); // delay loop
+		LED_ptr = 0x000003FF;
 
 		for (delay_count = 250000; delay_count != 0; --delay_count); // delay loop
+		LED_ptr = 0x00000000;
 	}
 }
