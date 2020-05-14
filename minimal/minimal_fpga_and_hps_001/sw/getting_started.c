765dbe59bb833f83d55a6bd65bede05631e2a78d
root <root@cobalt-vm.acct04.us.lmco.com>
2020-05-13 23:24:38 -0400

    working software with good clock period

--- minimal/minimal_fpga_and_hps_001/sw/getting_started.c
+++ minimal/minimal_fpga_and_hps_001/sw/getting_started.c
@@ -1,5 +1,5 @@
+#include <stdio.h>
 #include "address_map.h"
-
 /* This program demonstrates the use of parallel ports in the DE1-SoC Computer
  * It performs the following: 
  * 	1. displays the SW switch values on the red lights LEDR
@@ -8,17 +8,19 @@
 */
 int main(void)
 {
-	/* Declare volatile pointers to I/O registers (volatile means that the locations
-	 * will not be cached, even in registers) */
-	volatile int * LED_ptr 				= (int *) PIO_0_LED_BASE;
-	volatile int delay_count;					// volatile so the C compiler doesn't remove the loop
-
-	while (1)
-	{
-		for (delay_count = 250000; delay_count != 0; --delay_count); // delay loop
-		LED_ptr = 0x000003FF;
-
-		for (delay_count = 250000; delay_count != 0; --delay_count); // delay loop
-		LED_ptr = 0x00000000;
-	}
+    /* Declare volatile pointers to I/O registers (volatile means that the locations
+     * will not be cached, even in registers) */
+    //volatile int *LED = (int *) 0xFF200000; //PIO_0_LED_BASE;
+    volatile int *LED = (int *) 0xFF200010; //PIO_0_LED_BASE;
+    volatile int delay_count;					// volatile so the C compiler doesn't remove the loop
+    int delay_period = 2500000;
+    
+    while (1)
+    {
+        for (delay_count = delay_period; delay_count >= 0; delay_count--); // delay loop
+        *(LED) = 0x000003FF;
+        
+        for (delay_count = delay_period; delay_count >= 0; delay_count--); // delay loop
+        *(LED) = 0x00000000;
+    }
 }
