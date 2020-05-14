93dd5d24f4c3ff71a3d56674cc7ac33a9561d5c3
root <root@cobalt-vm.acct04.us.lmco.com>
2020-05-14 19:16:52 -0400

    generateing arm cortex a9 only

--- minimal/minimal_fpga_and_hps_001/generate_hps_qsys_header.sh
+++ minimal/minimal_fpga_and_hps_001/generate_hps_qsys_header.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-sopc-create-header-files "./soc_system.sopcinfo" #\
+
+#sopc-create-header-files "./soc_system.sopcinfo" #\
 #                         --single hps_0.h \
 #                         --module hps_0
+
+sopc-create-header-files "*.sopcinfo" --single hps_0_arm_a9_0.h --module hps_0_arm_a9_0
